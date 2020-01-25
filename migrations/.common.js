83eda77a57a968e282cbee7b1a4d81c148fb5889
Diego Pereira <diego_pereira@aol.com>
2020-01-25 16:29:46 -0300

    Update .common.js
    In migrations/.common.js on row 4, where KUE is handling the Redis, I needed to hardcode the settings for Redis:

--- migrations/.common.js
+++ migrations/.common.js
@@ -1,9 +1,21 @@
 
 const config = require('config')
 const kue = require('kue')
-const queue = kue.createQueue(Object.assign(config.kue, { redis: config.redis }))
-const es = require('../src/lib/elastic')
 const client = es.getClient(config)
+
+let queue = kue.createQueue(Object.assign(config.kue, { redis: {
+  host: "ec2-3-82-83-129.compute-1.amazonaws.com",
+  port: "15089",
+  auth: "p8a142f13cc2295df78ec831a7ea78cbfe62873a72698d8730ee9a959bd4784ad",
+  db: 0
+} }))
+
+const esConfig = {  
+  host: {
+    host: config.elasticsearch.host,
+    protocol: 'https',
+    port: config.elasticsearch.port
+  },
 
 exports.db = client
 exports.queue = queue
